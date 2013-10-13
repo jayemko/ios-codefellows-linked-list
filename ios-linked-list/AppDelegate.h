a170f06385498ede7fc98a0ac45ad54a86f7c5dc
Jason Koceja <jason@koceja.com>
2013-10-13 14:40:30 -0700

    initial commit

---
+++ ios-linked-list/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ios-linked-list
+//
+//  Created by Jason Koceja on 10/13/13.
+//  Copyright (c) 2013 CodeFellows. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ios-linked-list/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ios-linked-list
+//
+//  Created by Jason Koceja on 10/13/13.
+//  Copyright (c) 2013 CodeFellows. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
