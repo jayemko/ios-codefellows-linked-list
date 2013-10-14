d0261dbdb2f0e907e1c0f972257de8686fb35062
Jason Koceja <jason@koceja.com>
2013-10-13 17:33:53 -0700

    add tableview to display a sample linked list

---
+++ ios-linked-list/LLTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  LLTableViewController.h
+//  ios-linked-list
+//
+//  Created by Jason Koceja on 10/13/13.
+//  Copyright (c) 2013 CodeFellows. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LLTableViewController : UITableViewController
+
+@end
---
+++ ios-linked-list/LLTableViewController.m
@@ -0,0 +1,61 @@
+//
+//  LLTableViewController.m
+//  ios-linked-list
+//
+//  Created by Jason Koceja on 10/13/13.
+//  Copyright (c) 2013 CodeFellows. All rights reserved.
+//
+
+#import "LLTableViewController.h"
+
+@interface LLTableViewController ()
+
+@end
+
+@implementation LLTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    return 0;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+#warning Incomplete method implementation.
+    // Return the number of rows in the section.
+    return 0;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    // Configure the cell...
+    
+    return cell;
+}
+
+@end
